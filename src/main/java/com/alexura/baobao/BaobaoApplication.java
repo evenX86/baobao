6ffde2682c1ff847d115cebeb927401cc80ca04c
xuyifei <evenX86@gmail.com>
2019-03-04 18:33:26 +0800

    init commit

---
+++ src/main/java/com/alexura/baobao/BaobaoApplication.java
@@ -0,0 +1,13 @@
+package com.alexura.baobao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaobaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaobaoApplication.class, args);
+	}
+
+}
