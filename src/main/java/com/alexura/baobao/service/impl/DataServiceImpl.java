2e66433a04c5c05415b560e0bd5bdfde65153bbd
xuyifei <evenX86@gmail.com>
2019-05-12 23:58:51 +0800

    路径设置

--- src/main/java/com/alexura/baobao/service/impl/DataServiceImpl.java
+++ src/main/java/com/alexura/baobao/service/impl/DataServiceImpl.java
@@ -3,6 +3,9 @@ package com.alexura.baobao.service.impl;
 import com.alexura.baobao.entity.ActivityEntity;
 import com.alexura.baobao.mapper.ActivityMapper;
 import com.alexura.baobao.service.DataService;
+import com.alexura.baobao.utils.JsonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,7 @@ import java.util.Map;
  */
 @Service
 public class DataServiceImpl implements DataService {
+    private static final Logger log = LoggerFactory.getLogger(DataServiceImpl.class);
     @Autowired
     private ActivityMapper activityMapper;
     @Override
@@ -26,7 +30,22 @@ public class DataServiceImpl implements DataService {
 
     @Override
     public List<ActivityEntity> listActivity() {
-        return  activityMapper.getAll();
+        List<ActivityEntity> result = activityMapper.getAll();
+        for (ActivityEntity activityEntity : result) {
+            if (activityEntity.getActImg1() != null && !activityEntity.getActImg1().contains("static")) {
+                activityEntity.setActImg1("static/img/"+activityEntity.getActImg1());
+            }
+            if (activityEntity.getActImg2() != null && !activityEntity.getActImg2().contains("static")) {
+                activityEntity.setActImg2("static/img/"+activityEntity.getActImg2());
+            }
+            if (activityEntity.getActImg3() != null && !activityEntity.getActImg3().contains("static")) {
+                activityEntity.setActImg3("static/img/"+activityEntity.getActImg3());
+            }
+            if (activityEntity.getActImg4() != null && !activityEntity.getActImg4().contains("static")) {
+                activityEntity.setActImg4("static/img/"+activityEntity.getActImg4());
+            }
+        }
+        return  result;
     }
 
     @Override
